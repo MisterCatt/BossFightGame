b1d16fc2c4ef3c4d0aaaeb69ff346663704758ae
Mistercatt <mardunovich@gmail.com>
2025-02-19 16:49:44 +0100

    added dotween to project

--- BossFightGame/Assets/Scripts/Projectile/TimedDamageCircle/TimedDamageCircle.cs
+++ BossFightGame/Assets/Scripts/Projectile/TimedDamageCircle/TimedDamageCircle.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class TimedDamageCircle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+
+    public void OnEnable()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
